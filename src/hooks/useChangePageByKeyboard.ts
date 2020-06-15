30da51acfab4bd1bf194b204ce6402eee86a5e3e
linxianxi <904492381@qq.com>
2020-06-15 15:25:18 +0800

    fix(hooks): 修改翻页快捷键

--- src/hooks/useChangePageByKeyboard.ts
+++ src/hooks/useChangePageByKeyboard.ts
@@ -1,13 +1,13 @@
-import { useEffect } from "react";
 import keyboard from "keyboardjs";
+import { useEffect } from "react";
 
 export default function useChangePageByKeyboard(
   page: number,
   handle: (nextPage: number) => Promise<void>
 ) {
   useEffect(() => {
-    keyboard.bind("j", () => handle(page - 1));
-    keyboard.bind("k", () => handle(page + 1));
-    return () => keyboard.unbind(["j", "k"]);
+    keyboard.bind("command + j", () => handle(page - 1));
+    keyboard.bind("command + k", () => handle(page + 1));
+    return () => keyboard.unbind(["command + j", "command + k"]);
   }, [handle, page]);
 }
