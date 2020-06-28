d4cbaca5d8e020218a0f338b902d8ba3ff545747
Peatechen <1349234082@qq.com>
2020-06-28 11:37:46 +0800

    fix: 修复无法生成类型声明文件问题及stories报警告问题,升级 antd-simple-table
    修复无法生成类型声明文件问题及stories报警告问题,升级 antd-simple-table 到 1.4.2

--- src/interfaces/GraphQLTableColumnType.ts
+++ src/interfaces/GraphQLTableColumnType.ts
@@ -1,6 +1,7 @@
-import { FilterType } from "../types/FilterType";
 import { SimpleColumnType } from "antd-simple-table";
 
+import { FilterType } from "../types/FilterType";
+
 export interface GraphQLTableColumnType<T> extends SimpleColumnType<T> {
-  filterType?: FilterType;   
+  filterType?: FilterType;
 }
